d4f9655663343613c2287b8857cd8b06eb613169
trungtran <baotrung06092003@gmail.com>
2024-12-01 21:56:43 +0700

    feat: initial header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,19 @@
+const Header: React.FC = () => {
+  return (
+    <header className=''>
+      <div className=''>Logo here</div>
+      <div className='flex items-center gap-6'>
+        <div className=''>About</div>
+        <div className=''>Communities</div>
+        <div className=''>Workspace</div>
+        <div className=''>Price</div>
+      </div>
+      <div className=''>
+        <div className=''>Member Log In</div>
+        <div className=''>Contact us</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
